5a4015d4a0753f75e8bce011600e1f92da6214f5
Cristiane Delgado <krisMdelgado@gmail.com>
2024-06-20 21:45:39 -0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require("cypress");
 module.exports = defineConfig({
+  projectId: "1ozdz2",
   defaultCommandTimeout: 5000,  
   e2e: {
     baseUrl: 'https://automationexercise.com',
